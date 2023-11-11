abe8792542cba787725b044a7a5b4d6ac69dfb31
Federico Ezequiel Torres <t.federico.e@gmail.com>
2023-11-11 14:03:02 -0300

    feat: add test for extract class

--- tests/bcra_extract.py
+++ tests/bcra_extract.py
@@ -1,7 +1,7 @@
 from src.models.BCRA import BCRATransformer
 
 
-
+# TODO: define final production tests to validate working classes
 def test_BCRA_transformer_class():
      # extraction dates
     # ex_1 = BCRAExtractor('2023-02-01', testing_mode=False)
@@ -17,7 +17,7 @@ def test_BCRA_transformer_class():
     br = BCRATransformer(BCRA_data)
 
 
-
+# TODO: define final production tests to validate working classes
 def test_BCRA_extractor_class():
      # extraction dates
     # ex_1 = BCRAExtractor('2023-02-01', testing_mode=False)
@@ -31,3 +31,22 @@ def test_BCRA_extractor_class():
     
     # transformation raw to parsed
     # br = BCRATransformer(BCRA_data)
+
+
+
+
+
+
+
+from src.models.BCRA.bcra_extract import BCRAExtractor
+
+if __name__ == '__main__':
+    date = '20230301'
+    # TODO: opening test_file raises DateError
+    # Need to make available test_file only load into datevalidation logic
+    response = BCRAExtractor(test_file=r'C:\Users\Federico\Desktop\DE_ETL_1\src\data\BCRA\responses\mock_data_raw_1.html')
+    print(response)
+    
+    export_name = f'EXTRACTED_DATE_{date}.html'
+    with open(export_name, 'wb') as file:
+        file.write(response[date])
