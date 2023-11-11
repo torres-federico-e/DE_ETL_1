d80db84becbfa04ba8bc624c2b55a04e6f047af5
Federico Ezequiel Torres <t.federico.e@gmail.com>
2023-11-11 14:04:44 -0300

    chore: [WIP] refactor extract processor

--- src/models/BCRA/bcra_extract.py
+++ src/models/BCRA/bcra_extract.py
@@ -1,45 +1,20 @@
 
-#%% Data Extraction - Example script
-# --------------------------------------------------------
-
-# url = 'https://www.bcra.gob.ar/publicacionesestadisticas/Tipo_de_cambio_minorista_2.asp'
-# currency_code = '2'
-# date = '2022-03-01'
-
-# payload = {'moneda': currency_code, 'fecha': date}
-# response = requests.post(url, data=payload)
-# print(response.content)  
-# res = response.content
-
-
-# with open('data_raw_bcra_api.html', 'wb') as f:
-#     f.write(res)
-
-
-#%%   emulate main.py file for imports
-#----------------------------------------------------------
+# Standard lib
 import os
-
-if os.path.exists('../../../src'): 
-    os.chdir('../../../')
-
-
-#%% Imports
 from datetime import datetime, timedelta
 from typing import List, Dict
-import yaml
 
-from bs4 import BeautifulSoup
-import pandas as pd
+# Modules
 import requests
+from bs4 import BeautifulSoup
+import yaml
+import pandas as pd
 
-
+# internal
 from src.errors.bcra_errors import InvalidDateRequestError, InvalidDateFormatError
 
 
-
-#%% Typing aliases
-
+# Type definitions
 Response= str
 Date = str
 Raw_HTML = str
@@ -49,24 +24,34 @@ Parsed_HTML_tables = Dict[Date, BeautifulSoup]
 
 #%% Date processor class
 class DateProcessor:
-    '''Date processor class, handles interpretation, generation of calendar dates lists, and date lists export'''
+    '''Processor class, handles date interpretation, validation and generation of calendar ranges'''
     pass
+
+
 
 
 #%% Extractor Class
 class BCRAExtractor:
-    '''Processor class, handles requestss to BCRA API for specified dates.'''
+    '''Processor class, handles BCRA API requests to extract exchange rates from for specified dates.'''
 
-    # Official BCRA Endpoint
-    url_endpoint = 'https://www.bcra.gob.ar/publicacionesestadisticas/Tipo_de_cambio_minorista_2.asp'
-    # Accepted Currencies
-    currency_code = {'EUR': '98', 'USD':'2'}
+    # BCRA Endpoint
+    default_api_endpoint = 'https://www.bcra.gob.ar/publicacionesestadisticas/Tipo_de_cambio_minorista_2.asp'
+    
+    # Supported Currencies
+    currency_code = {
+                     'EUR': '98', 
+                     'USD':'2'
+                     }
 
     def __init__(self, date = None, test_file = None , * , start_date = None, end_date = None
                 ,config_path = 'src\config\ETL_config.yaml', mock = False 
                 ,locator_tag='table', **attributes):    
+        
         try: 
+            #------------------------------------------------------------
+            #TODO: Abstract Date validation to Date processor Class
             # Extraction validation
+            #------------------------------------------------------------
             self._is_valid_date_request(date, start_date, end_date)
             self._is_valid_date_format(date, start_date, end_date)  
             
@@ -77,25 +62,64 @@ class BCRAExtractor:
             print("Invalid date format: {}".format(str(e)))
             raise e
         
+            #-------------------------------------------------------------
+
+
         self.config = self.load_config_file(config_path)
         self.dates = self._get_date_list(date, start_date, end_date)
-        self.raw_html = self.get_html_reponse(self.dates, self.currency_code['USD']) 
+        self.raw_html = self.get_html_response() 
 
+    @classmethod
+    def extract_exchange_rates(cls, dates:List[Date], currency_code=None)\
+        -> Dict[Date, Raw_HTML]:
+        '''
+        Extracts exchange rates from  BCRA API for specific dates.
+        Returns dictionary with dates and raw HTML as response.
+         Returns -> Dict[Date, Raw_HTML]
+        '''
+        responses = dict()
+        for date in dates:
+            # `date`: %Y-%m-%d format already for API request and Dict keys
+            payload = {'moneda': currency_code, 'fecha': date}
+            response = requests.post(cls.default_api_endpoint, data=payload)
+            # Extend dictionary with new keys content
+            responses = {**responses, date:response.content}
+        return responses
 
+    def get_html_response(self) -> Dict[Date, Raw_HTML]:
+        '''Get HTML response from BCRA API service for configured dates
+          or from local test_file for debugging purposes'''
+        if self.test_file is None:        
+            responses = self.extract_exchange_rates(self.dates, self.currency_code)
+        elif self.test_file is not None:
+            # Check str is path + File
+            if os.path.isfile(self.test_file):
+                try: 
+                    mock_response = self.load_test_file()
+                    responses = mock_response if 'mock_response' in locals() else responses
+                except FileNotFoundError as e:
+                    raise e.add_note("Provided Test file Does not Exist")
+        return responses
+    
+    def load_test_file(self):
+        '''Loads test file as extraction response'''
+        with open(test_file, 'rb') as test_file:
+            test_file = test_file.read()
+        mock_response =  {self.dates[0]:test_file}
+        return mock_response
+    
     def load_config_file(self, config_path):
         '''Class config file loader'''
         with open(config_path) as f:
             config = yaml.safe_load(f)
         return config
-    
-    def load_mock_response(self):
-        '''Loads testing mock response'''
-        file_path = self.config.get('BCRA_extraction').get('test_file')
-        with open(file_path, 'rb') as test_file:
-            test_file = test_file.read()
-        mock_response =  {self.dates[0]:test_file}
-        return mock_response
-    
+
+# 
+# 
+# ...
+# 
+# 
+
     def _is_valid_date_request(self, date = None, start_date = None, end_date = None):
         '''Validates date requests for API'''
         if (date and (start_date or end_date )) or (not date and not (start_date and end_date)):
@@ -125,110 +149,19 @@ class BCRAExtractor:
             return dates
         elif date:
             return [date]
-    
-    def get_html_reponse(self, dates:List[Date] = None, currency_code=None) -> Dict[Date, Raw_HTML]:
-        if self.mock == False:        
-            responses = self.extract_exchange_rates(dates, currency_code)
-        elif self.mock == True:
-            mock_response = self.raw_html = self.load_mock_response()
-            responses = mock_response if 'mock_response' in locals() else responses
-        return responses
-    
-    def extract_exchange_rates(self, dates:List[Date], currency_code=None)-> Dict[Date, Raw_HTML]:
-        '''Makes API calls to BCRA API for the listed dates. 
-        Returns a Dictionary with dates as keys and responses as values.
-        * Return -> Dict[Date, Raw_HTML]
-        '''
-        responses = dict()
-        for date in dates:
-            # `date`: %Y-%m-%d format already for API request and Dict keys
-            payload = {'moneda': currency_code, 'fecha': date}
-            response = requests.post(self.url_endpoint, data=payload)
-            # Extend dictionary with new keys content
-            responses = {**responses, date:response.content}
-        return responses
-
-
-if __name__ == '__main__':
-    # Real Extraction Demo
-    ex_test = BCRAExtractor('2023-02-01', mock=True)
-    
-
-
-#%% Transformer Class
-class BCRATransformer:
-    '''Handles transormation between RAW HTML to 
-    Structured data for final target'''
-    
-    def __init__(self, extractor:BCRAExtractor, extract_filters = None):
-        self.extract_locators =  {'locator_tag':'table'} if extract_filters is None else extract_filters
-        self.responses = extractor.raw_html
-        self.parsed = self.html_parse(self.responses)
-        self.tables = self._extract_tables(self.parsed)
-        self.dfs = self.html_to_df(self.tables)
-        
-    def html_parse(self
-                , html_responses:Dict[Date, Raw_HTML]
-                , encoding:str = 'iso-8859-1'
-                , parser:str = 'html.parser') -> Dict[Date, BeautifulSoup]:
-        '''Takes Dict with dates as keys and Raw_HTML as values and
-        parses them into BeautifulSoup objects with 'html.parser' by default.
-        -> returns dict.'''
-        # _parsed = list()
-        _parsed = dict()
-        for date, response in html_responses.items():            
-            parsed_html = BeautifulSoup(response.decode(encoding=encoding), parser)
-            _parsed = {**_parsed, date:parsed_html}
-            # _parsed.append(parsed_html)
-        return _parsed
-
-    def _extract_tables(self, parsed_html: BeautifulSoup) -> Dict[Date, Parsed_HTML_tables]: 
-        '''Takes parsed BeautifulSoup objects and extracts the Exchange Rate tables.
-        Returns only filtered, matching HTML objects. 
-        returns -> Dict[Date, BeautifulSoup] '''
-        etag = self.extract_locators['tag']
-        eid = self.extract_locators['id']
-        for date, soup in parsed_html.items():
-            rate_table = soup.find(etag, {'id':eid})
-            result_tables = {**parsed_html, date:rate_table}
-        return result_tables
-
-    def html_to_df(self, date_tables: Dict[Date, Parsed_HTML_tables]) -> Dict[Date, pd.DataFrame]:
-        '''Takes BeautifulSoup parsed tables and transforms to Pandas Dataframe
-        returns -> Dict[Date, Dataframe]'''
-        final = dict()
-        for date, tables in date_tables.items():
-            table_list = []
-            for table in tables:
-                _df_table = pd.read_html(str(table))
-                table_list.append(_df_table)
-            final = {**final, date:table_list}
-        return final
-
-
-
-
-
-
-
-
-
-
-
 
 
     
-#%% USE CASES
+#%% Quickstart - Demo
 ##################################
 
-# from .data_models import BCRA_extractor
-# # Use cases:
+if __name__ == '__main__':
 
-# # Extract single date
-# res_extr = BCRA_extractor('2023-03-01') 
-# # Extract date range
-# res_extr = BCRA_extractor(start_date='2023-02-01', end_date='2023-02-28')
-# # extract and filter
-# res_extr = BCRA_extractor('2023-03-01', locator_tag='table', attr_filter={'id':'tablita'})
+    # Extract single date
+    single = BCRAExtractor('2023-03-01') 
+    # Extract date range
+    range_ = BCRAExtractor(start_date='2023-02-01', end_date='2023-02-28')
+    # extract and filter
+    filter_ = BCRAExtractor('2023-03-01', locator_tag='table', attr_filter={'id':'tablita'})
 
 
